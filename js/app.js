289efd5cdacf594abaa124ba74c838f4728c47cf
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-12-22 16:54:51 -0300

    salvar informação no localstorage

--- js/app.js
+++ js/app.js
@@ -13,15 +13,36 @@ const app = () => {
 
   const countNumbers = text => text.match(/\d/gim)?.length || 0;
 
+  const saveTextInLocal = text => {
+    window?.localStorage?.setItem('@contador', JSON.stringify(text));
+  };
+
+  const getTextFromLocal = () => {
+    return JSON.parse(window?.localStorage?.getItem('@contador'));
+  };
+
+  const updateCounters = inputText => {
+    $characters.textContent = countCharacters(inputText);
+    $words.innerHTML = countWords(inputText);
+    $numbers.innerHTML = countNumbers(inputText);
+  };
+
+  const retrieveInformation = () => {
+    const inputText = getTextFromLocal();
+    $textarea.value = inputText;
+    inputText && updateCounters(inputText);
+  };
+
   const init = () => {
+    retrieveInformation();
+
     $textarea.addEventListener('input', e => {
       e.preventDefault();
 
       const inputText = e.target.value || '';
 
-      $characters.textContent = countCharacters(inputText);
-      $words.innerHTML = countWords(inputText);
-      $numbers.innerHTML = countNumbers(inputText);
+      saveTextInLocal(inputText);
+      updateCounters(inputText);
     });
   };
 
